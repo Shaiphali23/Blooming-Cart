b9a6858cfe6e1279f829a7e866dc09466b5b6b27
Shaiphali23 <shaiphalijaiswal978@gmail.com>
2026-02-04 15:02:27 +0530

    Added Orders Page

---
+++ src/app/admin/orders/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+// Make sure you have a proper default export
+export default function OrdersPage() {
+  return (
+    <div>
+      {/* Your page content */}
+      <h1>Orders Page</h1>
+    </div>
+  );
+}
